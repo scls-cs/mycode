ffcc7f20a7ed4538d3b0878c68817e83a6cf6502
scls-cs <70875916+scls-cs@users.noreply.github.com>
2021-03-15 14:47:21 +0800

    MakeCode 项目的初始文件

---
+++ images.g.ts
@@ -0,0 +1,88 @@
+// 自动生成的代码。请勿编辑。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+...bbccccccbb...
+..bdddddddd1db..
+.bddbbbbbbbbddb.
+.cdb11111111bdc.
+.cbcdbbbbbbdcbc.
+.fbcc111111ccbf.
+.fbcd111111dcbf.
+f66cdd1111ddc66f
+f66ccbbbbbbcc66f
+fcbb33333333bbcf
+fbb3333333333bbf
+fbb3d111111d3bbf
+fbd1111111111dbf
+fdd1111111111ddf
+fdd1111111111ddf
+fdd1111111111ddf
+fdd1111111111ddf
+fdd11dbbbbd11ddf
+cdbbddddddddbbdf
+cbddddddddddddbc
+cddddddddddddddc
+.cccccccccccccc.
+.fbbfbbbbbbfbbf.
+..ff........ff..
+`;
+            case "image2":return img`
+. . . . . . . c d . . . . . . . 
+. . . . . . . c d . . . . . . . 
+. . . . . . . c d . . . . . . . 
+. . . . . . . c b . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . c 4 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . e 4 . . . . . . . 
+. . . . . . e e 5 2 . . . . . . 
+. . . . . . e 4 5 2 . . . . . . 
+. . . . . c c c 2 2 2 . . . . . 
+. . . . e e 4 4 4 5 2 2 . . . . 
+. . e f f f c c 2 2 f f 2 2 . . 
+. e e e e 2 2 4 4 4 4 5 4 2 2 . 
+e e e e e e 2 2 4 4 4 5 4 4 2 2 
+e e e e e e 2 2 4 4 4 4 5 4 2 2 
+`;
+            case "image3":return img`
+........................
+........................
+........................
+........................
+..........ffff..........
+........ff1111ff........
+.......fb111111bf.......
+.......f11111111f.......
+......fd11111111df......
+......fdd111111ddf......
+......fbdd1111dddf......
+......fcdbfddfbdbf......
+.......fbcf11fcbfff.....
+.......ffb1111bcfbcf....
+........fcdb1bdfbbbf....
+.......ffffffffffcf.....
+.....fcb1bcfffff........
+.....f1b1b1ffff.........
+......ffbff.............
+........................
+........................
+........................
+........................
+........................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
